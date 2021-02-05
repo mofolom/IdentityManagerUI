5671cfeac52893f062138cea55a824ec4797fe92
Mofolo <mofolo@hotmail.com>
2021-02-05 05:17:10 +0200

    init

--- IdentityManagerUI/Areas/IdentityManager/Models/ApplicationUser.cs
+++ IdentityManagerUI/Areas/IdentityManager/Models/ApplicationUser.cs
@@ -8,6 +8,8 @@ namespace IdentityManagerUI.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string IdentityNumber { get; set; }
+        public string CellphoneNumber { get; set; }
         public virtual ICollection<IdentityUserRole<string>> Roles { get; set; }
         public virtual ICollection<IdentityUserClaim<string>> Claims { get; set; }
     }
